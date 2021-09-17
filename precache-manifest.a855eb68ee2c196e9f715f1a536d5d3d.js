702e3a888b5b71aaa06fadd0fb30100c9c67d119
Liad Zigdon <liadzigdon@Liads-MBP.lan>
2021-09-17 13:32:28 +0300

    Updates

---
+++ precache-manifest.a855eb68ee2c196e9f715f1a536d5d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7095b218b460b95c77339e32ec3477a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6a9c49ce9f8b215588d",
+    "url": "./static/css/main.0736c991.chunk.css"
+  },
+  {
+    "revision": "9ea393c4b93ddf553159",
+    "url": "./static/js/2.b69cd68c.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.b69cd68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a9c49ce9f8b215588d",
+    "url": "./static/js/main.26f1ce22.chunk.js"
+  },
+  {
+    "revision": "f24136b7195886dc5a9d",
+    "url": "./static/js/runtime-main.598756de.js"
+  }
+]);
